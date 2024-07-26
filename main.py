9e6459b4cb9ad656fe5d62633a8e35132c36fdc2
GuGraciano <gustavograciano477@gmail.com>
2024-07-26 15:14:54 -0300

    terminando

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def inicial():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/cardapio')
+def cardapio():
+    return render_template('cardapio.html')
+
+
 
 if __name__ == '__main__':
     app.run()
-
